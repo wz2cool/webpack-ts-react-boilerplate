aa118bf8af3606a9f25755a6b38a203945a1ce73
wz2cool <wz2cool@live.cn>
2023-03-26 16:10:51 +0800

    add

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,22 +1,22 @@
- 
 import React from "react";
-import { createBrowserRouter, createRoutesFromElements, Outlet, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+} from "react-router-dom";
 import ErrorPage from "../error-page";
 import About from "../pages/about";
 import Home from "../pages/home";
- 
+
 import Root from "./root";
 
- 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route path="home" element={<Home />} />
-      <Route path="about" element={<About />} />
- 
+    <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+      <Route path="home" element={<Home />} errorElement={<ErrorPage />} />
+      <Route path="about" element={<About />} errorElement={<ErrorPage />} />
     </Route>
   )
 );
 export default router;
-
-
